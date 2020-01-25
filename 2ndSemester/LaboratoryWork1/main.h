850070ab10e75c8d60090f6acea787949935c419
Ihor Tarkhan <igor.tark@gmail.com>
2020-01-25 12:56:23 +0200

    Lab 2-1 INIT

---
+++ 2ndSemester/LaboratoryWork1/main.h
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main_Lab_2_1() {
+	cout << "Lab_2_1";
+}
--- MAIN.cpp
+++ MAIN.cpp
@@ -3,7 +3,8 @@
 #include "1stSemester/LaboratoryWork3/main.h"
 #include "1stSemester/LaboratoryWork4/main.h"
 #include "1stSemester/LaboratoryWork5/main.h"
+#include "2ndSemester/LaboratoryWork1/main.h"
 
 int main() {
-    main_Lab_1_4();
+    main_Lab_2_1();
 }
