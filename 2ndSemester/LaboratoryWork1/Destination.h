97fdd8d50cc4e37a99b1e07d5f807188ff45eb41
Ihor Tarkhan <igor.tark@gmail.com>
2020-01-28 13:05:11 +0200

    add Date (example - 12:48, 15.07.2019); add enum Destination

---
+++ 2ndSemester/LaboratoryWork1/Destination.h
@@ -0,0 +1,13 @@
+#ifndef HOMEWORK_DESTINATION_H
+#define HOMEWORK_DESTINATION_H
+
+
+enum Destination{
+    INTERCITY,
+    FAST,
+    SIMPLE,
+    NEARCITY
+};
+
+
+#endif
---
+++ 2ndSemester/LaboratoryWork1/Train.h
@@ -0,0 +1,18 @@
+#ifndef HOMEWORK_TRAIN_H
+#define HOMEWORK_TRAIN_H
+
+#include <iostream>
+#include "Date.h"
+
+using namespace std;
+
+class Train {
+    int number;
+    string optionalName;
+    Date arrive;
+    Date departure;
+    double rate;
+};
+
+
+#endif
