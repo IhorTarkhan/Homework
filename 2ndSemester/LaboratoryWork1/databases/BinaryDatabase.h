5cbf3ad64276045f60be16a9041472d25ef0ea2a
Ihor Tarkhan <igor.tark@gmail.com>
2020-02-10 17:32:55 +0200

    write to bin and read from it

--- 2ndSemester/LaboratoryWork1/databases/BinaryDatabase.h
+++ 2ndSemester/LaboratoryWork1/databases/BinaryDatabase.h
@@ -14,11 +14,6 @@ private:
 
 public:
 
-    static void refresh_file() {
-        ofstream file(PATH);
-        file.close();
-    }
-
     static void saveMemoryToIt() {
     }
 
@@ -26,39 +21,161 @@ public:
     }
 
     static int getMaxID() {
+        int maxID = 0;
+//        string line;
+//        ifstream file(BinaryDatabase::PATH);
+//        while (getline(file, line)) {
+//            if (line.substr(0, ID.length()) == ID) {
+//                if (stoi(line.substr(ID.length())) > maxID)
+//                    maxID = stoi(line.substr(ID.length()));
+//            }
+//        }
+        return maxID;
     }
 
     static void saveTrain(Train train) {
+        int maxID = getMaxID() + 1;
         ofstream file(BinaryDatabase::PATH, ios_base::app | ios_base::binary);
-        size_t size = sizeof(train.number);
-        file.write((char *) &train, sizeof(Train));
-        cout << train.getInText() << endl;
+
+        file.write((char *) &(maxID), sizeof(maxID));
+
+        size_t numberSize = train.number.size();
+        file.write((char *) &numberSize, sizeof(numberSize));
+        const char *numberBuffer = train.number.c_str();
+        file.write(numberBuffer, numberSize);
+
+        size_t nameSize = train.optionalName.size();
+        file.write((char *) &nameSize, sizeof(nameSize));
+        const char *nameBuffer = train.optionalName.c_str();
+        file.write(nameBuffer, nameSize);
+
+        file.write((char *) &(train.destination), sizeof(train.destination));
+
+
+        file.write((char *) &(train.arrive.hour), sizeof(train.arrive.hour));
+        file.write((char *) &(train.arrive.minute), sizeof(train.arrive.minute));
+        file.write((char *) &(train.arrive.day), sizeof(train.arrive.day));
+        file.write((char *) &(train.arrive.month), sizeof(train.arrive.month));
+        file.write((char *) &(train.arrive.year), sizeof(train.arrive.year));
+
+        file.write((char *) &(train.departure.hour), sizeof(train.departure.hour));
+        file.write((char *) &(train.departure.minute), sizeof(train.departure.minute));
+        file.write((char *) &(train.departure.day), sizeof(train.departure.day));
+        file.write((char *) &(train.departure.month), sizeof(train.departure.month));
+        file.write((char *) &(train.departure.year), sizeof(train.departure.year));
+
+
+        file.write((char *) &(train.rate), sizeof(train.rate));
+
         file.close();
     }
 
     static string getTrains() {
         vector<Train> trains;
         ifstream file(BinaryDatabase::PATH, ios_base::binary);
-        Train train;
-        while (!file.eof()) {
-            if (file.eof()) { break; }
-            file.read((char *) &train, sizeof(Train));
-            if (file.eof()) { break; }
-            cout << train.getInText() << endl;
-            trains.push_back(train);
-        }
-
         string trainsInString = "";
-        for (int i = 0; i < trains.size(); ++i) {
-            trainsInString += trains[i].getInText();
+
+        while (!file.eof()) {
+            int trainID;
+            Train train;
+
+            file.read((char *) &(trainID), sizeof(trainID));
+            if (file.eof()) { break; }
+
+            size_t numberSize;
+            file.read((char *) &numberSize, sizeof(numberSize));
+            char *numberBuffer = new char[numberSize + 1];
+            file.read(numberBuffer, numberSize);
+            numberBuffer[numberSize] = '\0';
+            train.number = numberBuffer;
+
+            size_t nameSize;
+            file.read((char *) &nameSize, sizeof(nameSize));
+            char *nameBuffer = new char[nameSize + 1];
+            file.read(nameBuffer, nameSize);
+            nameBuffer[nameSize] = '\0';
+            train.optionalName = nameBuffer;
+
+            file.read((char *) &(train.destination), sizeof(train.destination));
+
+
+            file.read((char *) &(train.arrive.hour), sizeof(train.arrive.hour));
+            file.read((char *) &(train.arrive.minute), sizeof(train.arrive.minute));
+            file.read((char *) &(train.arrive.day), sizeof(train.arrive.day));
+            file.read((char *) &(train.arrive.month), sizeof(train.arrive.month));
+            file.read((char *) &(train.arrive.year), sizeof(train.arrive.year));
+
+            file.read((char *) &(train.departure.hour), sizeof(train.departure.hour));
+            file.read((char *) &(train.departure.minute), sizeof(train.departure.minute));
+            file.read((char *) &(train.departure.day), sizeof(train.departure.day));
+            file.read((char *) &(train.departure.month), sizeof(train.departure.month));
+            file.read((char *) &(train.departure.year), sizeof(train.departure.year));
+
+
+            file.read((char *) &(train.rate), sizeof(train.rate));
+
+            trains.push_back(train);
+            trainsInString += train.getInText() + "\n";
         }
+        file.close();
         return trainsInString;
     }
 
     static void clearTrains() {
+        ofstream file(PATH);
+        file.close();
     }
 
     static string getTrainsWithID() {
+        vector<Train> trains;
+        ifstream file(BinaryDatabase::PATH, ios_base::binary);
+        string trainsInString = "";
+
+        while (!file.eof()) {
+            int trainID;
+            Train train;
+
+            file.read((char *) &(trainID), sizeof(trainID));
+            if (file.eof()) { break; }
+
+            size_t numberSize;
+            file.read((char *) &numberSize, sizeof(numberSize));
+            char *numberBuffer = new char[numberSize + 1];
+            file.read(numberBuffer, numberSize);
+            numberBuffer[numberSize] = '\0';
+            train.number = numberBuffer;
+
+            size_t nameSize;
+            file.read((char *) &nameSize, sizeof(nameSize));
+            char *nameBuffer = new char[nameSize + 1];
+            file.read(nameBuffer, nameSize);
+            nameBuffer[nameSize] = '\0';
+            train.optionalName = nameBuffer;
+
+            file.read((char *) &(train.destination), sizeof(train.destination));
+
+
+            file.read((char *) &(train.arrive.hour), sizeof(train.arrive.hour));
+            file.read((char *) &(train.arrive.minute), sizeof(train.arrive.minute));
+            file.read((char *) &(train.arrive.day), sizeof(train.arrive.day));
+            file.read((char *) &(train.arrive.month), sizeof(train.arrive.month));
+            file.read((char *) &(train.arrive.year), sizeof(train.arrive.year));
+
+            file.read((char *) &(train.departure.hour), sizeof(train.departure.hour));
+            file.read((char *) &(train.departure.minute), sizeof(train.departure.minute));
+            file.read((char *) &(train.departure.day), sizeof(train.departure.day));
+            file.read((char *) &(train.departure.month), sizeof(train.departure.month));
+            file.read((char *) &(train.departure.year), sizeof(train.departure.year));
+
+
+            file.read((char *) &(train.rate), sizeof(train.rate));
+
+            trains.push_back(train);
+            trainsInString += ID + to_string(trainID) + "\n";
+            trainsInString += train.getInText() + "\n";
+        }
+        file.close();
+        return trainsInString;
     }
 
     static void changeTrain(int id, Train train) {
--- 2ndSemester/LaboratoryWork1/main.h
+++ 2ndSemester/LaboratoryWork1/main.h
@@ -2,14 +2,9 @@
 #include "workInConsole/standardUserWork/DatabaseConsole.h"
 
 int main_Lab_2_1() {
-    BinaryDatabase::refresh_file();
-    cout << endl;
+    BinaryDatabase::clearTrains();
     BinaryDatabase::saveTrain(createRandomTrain());
     BinaryDatabase::saveTrain(createRandomTrain());
     BinaryDatabase::saveTrain(createRandomTrain());
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    cout << BinaryDatabase::getTrains();
+    cout << BinaryDatabase::getTrainsWithID();
 }
