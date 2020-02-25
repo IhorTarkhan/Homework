fe34ab4d1e48123a9ce62799d9694a3f98274a25
Ihor Tarkhan <igor.tark@gmail.com>
2020-02-25 11:20:18 +0200

    rename directory LaboratoryWork2  --->>>  LaboratoryWork2a

--- MAIN.cpp
+++ MAIN.cpp
@@ -4,11 +4,8 @@
 #include "1stSemester/LaboratoryWork4/main.h"
 #include "1stSemester/LaboratoryWork5/main.h"
 #include "2ndSemester/LaboratoryWork1/main.h"
-#include "2ndSemester/LaboratoryWork2/main.h"
+#include "2ndSemester/LaboratoryWork2a/main.h"
 
 int main() {
     main_Lab_2_2();
-//    for (int i = 0; i < 250; ++i) {
-//        cout << "\u001B[" + to_string(i) + "m" << i << endl;
-//    }
 }
