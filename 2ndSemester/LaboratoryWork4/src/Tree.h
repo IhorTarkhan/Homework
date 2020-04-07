6d31f7610a520df23fb3abb46bd0ff814d8d6be9
ihorTarkhan <igor.tark@gmail.com>
2020-04-07 16:42:10 +0300

    Realised Block-3(deleting)

--- 2ndSemester/LaboratoryWork4/src/Tree.h
+++ 2ndSemester/LaboratoryWork4/src/Tree.h
@@ -1,32 +1,66 @@
 #ifndef HOMEWORK_TREE_H
 #define HOMEWORK_TREE_H
 
-#include "TreeNode.h"
+#include <string>
+#include <stdexcept>
+#include "vector"
+
+using namespace std;
 
 class Tree {
 public:
-    TreeNode *root;
+    int value;
+    vector<Tree *> posterity;
 
-    explicit Tree(TreeNode *root) {
-        this->root = root;
+    explicit Tree(int value) {
+        this->value = value;
     }
 
-    void add(TreeNode *node, int value, double p) {
+    string getOutputUsingParentheses() {
+        string outputData = to_string(value);
+        if (!posterity.empty()) {
+            outputData += "(";
+            for (int i = 0; i < posterity.size(); ++i) {
+                outputData += posterity[i]->getOutputUsingParentheses();
+                if (i != posterity.size() - 1) {
+                    outputData += ", ";
+                }
+            }
+            outputData += ")";
+        }
+        return outputData;
+    }
+
+    void add(Tree *newNode, double p) {
         if (p < 0 || p > 1) {
             throw invalid_argument("Incorrect random value p.");
         }
 
-        double randValue = ((double) rand() / RAND_MAX);
-        if (randValue <= p || node->posterity.empty()) {
-            node->posterity.push_back(new TreeNode(value));
+        double randValue = (double) rand() / RAND_MAX;
+        if (randValue <= p || posterity.empty()) {
+            posterity.push_back(newNode);
         } else {
-            int randomElementIndex = rand() % node->posterity.size();
-            add(node->posterity[randomElementIndex], value, p);
+            int randomElementIndex = rand() % posterity.size();
+            posterity[randomElementIndex]->add(newNode, p);
         }
     }
 
-    string outputUsingParentheses() {
-        return root->getOutputUsingParentheses();
+    void deleteNode(int value, double p) {
+        if (posterity.empty()) {
+            return;
+        }
+        for (int i = 0; i < posterity.size(); ++i) {
+            if (posterity[i]->value == value) {
+                vector<Tree *> reservePosterity = posterity[i]->posterity;
+                posterity.erase(posterity.begin() + i);
+
+                for (int j = 0; j < reservePosterity.size(); ++j) {
+                    add(reservePosterity[j], p);
+                }
+                deleteNode(value, p);
+            }
+            posterity[i]->deleteNode(value, p);
+        }
     }
 };
 
--- 2ndSemester/LaboratoryWork4/src/main.h
+++ 2ndSemester/LaboratoryWork4/src/main.h
@@ -4,10 +4,33 @@
 using namespace std;
 
 int main_Lab_2_4() {
-    Tree tree(new TreeNode(0));
+    Tree *root = new Tree(0);
     for (int i = 1; i < 20; ++i) {
-        tree.add(tree.root, i, 0.5);
+        root->add(new Tree(i), 0.9999999);
     }
-    cout << tree.outputUsingParentheses();
+    Tree *tree5_1 = new Tree(5);
+    tree5_1->add(new Tree(6), 0.9999999);
+    tree5_1->add(new Tree(5), 0.9999999);
+    tree5_1->add(new Tree(7), 0.9999999);
+    tree5_1->add(new Tree(6), 0.9999999);
+    Tree *tree5_2 = new Tree(2);
+    Tree *tree5_3 = new Tree(2);
+    Tree *tree5_4 = new Tree(4);
+    Tree *tree5_5 = new Tree(6);
+    Tree *tree5_6 = new Tree(7);
+    Tree *tree5_7 = new Tree(8);
+    Tree *tree5_55 = new Tree(5);
+    root->add(tree5_1, 0.9999999);
+    root->add(tree5_2, 0.9999999);
+    root->add(tree5_3, 0.9999999);
+    root->add(tree5_4, 0.9999999);
+    root->add(tree5_5, 0.9999999);
+    root->add(tree5_6, 0.9999999);
+    root->add(tree5_7, 0.9999999);
+    root->add(tree5_55, 0.9999999);
+
+    cout << root->getOutputUsingParentheses() << endl;
+    root->deleteNode(5, 0.99999999999);
+    cout << root->getOutputUsingParentheses() << endl;
     return 0;
 }
