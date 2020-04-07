9bcfd0d0760ff458ab1b22989de5d22c14e5fac4
ihorTarkhan <igor.tark@gmail.com>
2020-04-07 23:35:06 +0300

    demonstration-mode added

--- 2ndSemester/LaboratoryWork4/src/Interface.h
+++ 2ndSemester/LaboratoryWork4/src/Interface.h
@@ -20,6 +20,7 @@ public:
         cout << "Block -1 : add to the BINARY root new node" << endl;
         cout << "Block  5 : view BINARY tree" << endl;
         cout << "Block -2 : view BINARY tree as simple Tree (by parentheses)" << endl;
+        cout << "Your choice: ";
         int n;
         double p;
         cin >> n;
@@ -68,7 +69,133 @@ public:
     }
 
     static void demon() {
+        const string BlueInConsole = "\u001B[34m";
+        const string YellowInConsole = "\u001B[33m";
+        const string StandardConsole = "\u001B[0m";
+        int n_0 = 0;
 
+        Tree *root;
+        BinaryTree *binaryRoot;
+
+        cout << BlueInConsole << "TREE" << StandardConsole << endl;
+
+        cout << "Block  0 : Create new root, with value = 0" << endl;
+        root = new Tree(0);
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  1 : add to the root new nodes: from 1 to 20, with p = 0.5" << endl;
+        for (int i = 1; i <= 20; ++i) {
+            root->add(new Tree(i), 0.5);
+        }
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  2 : output the tree starting from root:" << endl;
+        cout << root->getOutputUsingParentheses() << endl;
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  3 : delete all nodes with value = 5" << endl;
+        root->deleteNode(5, 1);
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  2 : output the tree starting from root:" << endl;
+        cout << root->getOutputUsingParentheses() << endl;
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  0 : Create new root, with value = -1" << endl;
+        root = new Tree(-1);
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  1 : add to the root new nodes: from 1 to 10 and ten times 5, with p = 0.5" << endl;
+        for (int i = 1; i <= 10; ++i) {
+            root->add(new Tree(i), 0.5);
+            root->add(new Tree(5), 0.5);
+        }
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  3 : delete all nodes with value = 5" << endl;
+        root->deleteNode(5, 1);
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  2 : output the tree starting from root:" << endl;
+        cout << root->getOutputUsingParentheses() << endl;
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+
+        cout << "Block  0 : Create new root, with value = 0" << endl;
+        root = new Tree(0);
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  1 : add to the root new nodes: from 1 to 20, with p = 0.1" << endl;
+        for (int i = 1; i <= 20; ++i) {
+            root->add(new Tree(i), 0.1);
+        }
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  2 : output the tree starting from root:" << endl;
+        cout << root->getOutputUsingParentheses() << endl;
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+
+        cout << "Block  0 : Create new root, with value = 0" << endl;
+        root = new Tree(0);
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  1 : add to the root new nodes: from 1 to 20, with p = 0.9" << endl;
+        for (int i = 1; i <= 20; ++i) {
+            root->add(new Tree(i), 0.9);
+        }
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  2 : output the tree starting from root:" << endl;
+        cout << root->getOutputUsingParentheses() << endl;
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << endl;
+
+
+        cout << BlueInConsole << "BINARY-TREE" << StandardConsole << endl;
+
+        cout << "Block  4 : Create new BINARY root, with value = 10" << endl;
+        binaryRoot = new BinaryTree(10);
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block 4.1: add to the BINARY root new nodes: from 1 to 20" << endl;
+        for (int i = 1; i <= 20; ++i) {
+            binaryRoot->add(new BinaryTree(i));
+        }
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block 5.0: view BINARY tree as simple Tree (by parentheses)" << endl;
+        cout << binaryRoot->getOutputUsingParentheses() << endl;
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  5 : view BINARY tree" << endl;
+        cout << "/// Format of Presentation: ('value', 'rightIndex', 'isLeft')" << endl;
+        cout << binaryRoot->consistentPresentation(n_0) << endl;
+        n_0 = 0;
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  4 : Create new BINARY root, with value = 10" << endl;
+        binaryRoot = new BinaryTree(10);
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block 4.1: add to the BINARY root 20 new nodes: random between 1 and 20" << endl;
+        for (int i = 1; i <= 20; ++i) {
+            binaryRoot->add(new BinaryTree(rand() % 20 + 1));
+        }
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block 5.0: view BINARY tree as simple Tree (by parentheses)" << endl;
+        cout << binaryRoot->getOutputUsingParentheses() << endl;
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << "Block  5 : view BINARY tree" << endl;
+        cout << "/// Format of Presentation: ('value', 'rightIndex', 'isLeft')" << endl;
+        cout << binaryRoot->consistentPresentation(n_0) << endl;
+        n_0 = 0;
+        cout << YellowInConsole << "Done" << StandardConsole << endl << endl;
+
+        cout << endl;
     }
 };
 
