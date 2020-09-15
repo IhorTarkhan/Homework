070fa7a3469d29f122d50280f9e1cb6cc4f2b3af
IhorTarkhan <igor.tark@gmail.com>
2020-09-15 12:28:38 +0300

    RightAngle function for figure implemented

--- 3rdSemester/LaboratoryWork1/data/Figure.h
+++ 3rdSemester/LaboratoryWork1/data/Figure.h
@@ -10,7 +10,7 @@ class Point {
 private:
     double x, y;
 public:
-    Point() {}
+    Point() = default;
 
     Point(double x, double y) : x(x), y(y) {}
 
@@ -89,6 +89,15 @@ std::string toString(Figure element) {
 }
 
 /**
+ * additional function to realise other compare functions
+ * @return length between two points
+ */
+double getLength(Point p1, Point p2) {
+    return pow(pow(p1.getX() - p2.getX(), 2) +
+               pow(p1.getY() - p2.getY(), 2), 0.5);
+}
+
+/**
  * implement functions to check Square
  */
 bool isEqualSquare(Figure element) {
@@ -109,15 +118,32 @@ bool isEqualSquare(Figure element) {
  */
 bool isRight(Figure element) {
     Point *vertexes = element.getCoordinates();
-    double edgePow2 =
-            pow(vertexes[element.getNumberOfVertex() - 1].getX() - vertexes[0].getX(), 2) +
-            pow(vertexes[element.getNumberOfVertex() - 1].getY() - vertexes[0].getY(), 2);
+    double edgeLength = getLength(vertexes[element.getNumberOfVertex() - 1], vertexes[0]);
     for (int i = 0; i < element.getNumberOfVertex() - 1; ++i) {
-
-        if (edgePow2 - 1 > 0.01)
+        double edgeIterableLength = getLength(vertexes[i], vertexes[i + 1]);
+        if (edgeLength - edgeIterableLength > 0.01)
             return false;
     }
     return true;
+}
+
+/**
+ * implement functions to check Rectangular figure
+ */
+bool isContainRightAngle(Figure element) {
+    Point *vertexes = element.getCoordinates();
+    int vertexNumber = element.getNumberOfVertex();
+    for (int i = 0; i < vertexNumber + 3; ++i) {
+        if (
+                pow(getLength(vertexes[i], vertexes[(i + 1) % vertexNumber]), 2) +
+                pow(getLength(vertexes[i + 1], vertexes[(i + 2) % vertexNumber]), 2) -
+                pow(getLength(vertexes[i], vertexes[(i + 2) % vertexNumber]), 2)
+                > 0.01
+                ) {
+            return true;
+        }
+    }
+    return false;
 }
 
 #endif
